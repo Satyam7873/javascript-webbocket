6ac090187f5bc5daa1417798cafdd9f5324c42e9
Satyam7873 <152000119+Satyam7873@users.noreply.github.com>
2023-11-28 10:19:29 +0000

    introductionfile saved

---
+++ 01-basics/test1.js
@@ -0,0 +1,4 @@
+/* javascript is a high level scripting language to use on developing web and mobile application efficiently.
+we use javascript on client side as well as server side.
+in javascript we do logical implementain in webpages.
+in today era we use es6(eshma script 6) version javascript.(vanila javascript)
